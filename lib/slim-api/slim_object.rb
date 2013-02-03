c6c202844ed8b315df6a96ef0336a521ce7cd08d
Ondrej Bartas <ondrej@bartas.cz>
2013-02-03 12:47:39 +0100

    [FIX] Response - parse json into symbolized hash - update creation of new object

--- lib/slim-api/slim_object.rb
+++ lib/slim-api/slim_object.rb
@@ -29,23 +29,23 @@ module SlimApi
 
       def get id
         response = request(:get, id)
-        if response["status"] == "ok"
+        if response[:status] == "ok"
           new(response["#{self::NAME.to_s}"])
         else
-          raise "#{response["error_type"]} - #{response["message"]}"
+          raise "#{response[:error_type]} - #{response[:message]}"
         end
       end
 
       def find args = {}
         find_args = SlimApi.find_options.merge(args)
         response = request(:get, find_args)
-        if response["status"] == "ok"
+        if response[:status] == "ok"
           out = response["#{self::NAME.to_s.pluralize}"].collect{ |arg|
             new(arg.symbolize_keys).exists!
           }
           array = SlimArray.new out
           array.find_options = find_args
-          array.total_count = response["total_count"]
+          array.total_count = response[:total_count]
           array.limit = find_args[:limit]
           array.offset = find_args[:offset]
           array.find_object = self
@@ -69,6 +69,8 @@ module SlimApi
         when :get then
           if params && params.is_a?(Fixnum)
             curl.url = url+"/#{params}"
+          elsif params && params.has_key?(:id)
+            curl.url = url+"/#{params[:id]}"
           else
             curl.url = url+ (params ? "?#{params.to_query}" : "")
           end
@@ -86,7 +88,7 @@ module SlimApi
         end
 
         curl.http verb.to_s.upcase
-        response = Yajl::Parser.parse(curl.body_str)
+        response = Yajl::Parser.parse(curl.body_str, symbolize_keys: true)
       end
 
     end 
@@ -106,35 +108,35 @@ module SlimApi
           update
         else
           response = self.class.request(:post, self)
-          if response["status"] == "ok"
+          if response[:status] == "ok"
             exists!
-            if response["created"].is_a?(Hash)
-              self.id = response["created"]["id"]
+            if response[self.class::NAME].is_a?(Hash)
+              self.id = response[self.class::NAME][:id]
             end
             true
-          elsif response["error_type"] == "ApiError::BadRequest"
-            if response["errors"].is_a?(Hash)
-              @errors = response["errors"]["_errors"]
+          elsif response[:error_type] == "ApiError::BadRequest"
+            if response[self.class::NAME] && response[self.class::NAME][:_errors]
+              @errors = response[self.class::NAME][:_errors]
             else
-              @errors = response["message"]
+              @errors = response[:message]
             end
             false
           else
-            raise "#{response["error_type"]} - #{response["message"]}"
+            raise "#{response[:error_type]} - #{response[:message]}"
           end
         end
       end
 
       def update
         response = self.class.request(:put, self)
-        if response["status"] == "ok"
-          self.id = response["#{self.class::NAME}"]["id"]
+        if response[:status] == "ok"
+          self.id = response[self.class::NAME][:id]
           true
-        elsif response["error_type"] == "ApiError::BadRequest"
-          @errors = response["message"]
+        elsif response[:error_type] == "ApiError::BadRequest"
+          @errors = response[:message]
           false
         else
-          raise "#{response["error_type"]} - #{response["message"]}"
+          raise "#{response[:error_type]} - #{response[:message]}"
         end
       end
 
