aa965a4f9f826d0b0166a386aad555742bc2a83f
Ondrej Bartas <ondrej@bartas.cz>
2013-02-05 14:59:13 +0100

    [ADD] CTR, CPC, CPC_CZK, CPC_EUR for statistics to easier count these values

--- lib/slim-api/objects/statistics.rb
+++ lib/slim-api/objects/statistics.rb
@@ -7,5 +7,22 @@ module SlimApi
 
     NAME = :statistics
 
+    def cpc
+      return 0.0 if self.clicks.to_f == 0
+      (self.price_currency.to_f/self.clicks.to_f) rescue 0.0
+    end
+
+    def cpc_czk
+      self.price_czk.to_f/self.clicks.to_f rescue 0.0
+    end
+
+    def cpc_eur
+      self.price_eur.to_f/self.clicks.to_f rescue 0.0
+    end
+
+    def ctr
+      self.clicks.to_f/self.impressions.to_f*100.0 rescue 0.0
+    end
+
   end
 end
